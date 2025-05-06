9005a72a6646a1fc7f181626d84fd0c70cb6d134
Md-Azam <faisalazam1999@gmail.com>
2025-05-06 10:16:46 +0530

    practice some stream api logical questions

--- src/streams/StreamBasic.java
+++ src/streams/StreamBasic.java
@@ -1,7 +1,9 @@
 
 package streams;
 import java.util.*;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamBasic {
     public static void main(String[] args) {
@@ -15,6 +17,27 @@ public class StreamBasic {
     	List<String> fruits = Arrays.asList("Apple","Mango","Banana","Apple");
     	List<String> fruiltInStream = fruits.stream().distinct().toList();
     	System.out.println("fruiltInStream: "+ fruiltInStream);
+    	
+    	//Event number in list of integers:
+    	List<Integer> evens = list.stream().filter(num -> num%2==0).toList();
+    	System.out.println(evens);
+    	
+    	//Odd number in list of integers:
+    	List<Integer> odds = list.stream().filter(num -> num%2!=0).collect(Collectors.toList());
+    	System.out.println(odds);
+    	
+    	//Sum off all elements :
+    	int sum = list.stream().mapToInt(Integer :: intValue).sum();
+    	System.out.println(sum);
+    	
+    	//Search Char in a List of string
+    	List<String> upperCaseStrings = fruits.stream()
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+    	System.out.println("upperCaseStrings "+ upperCaseStrings);
+    	char toFind = 'A';
+    	long count = upperCaseStrings.stream().filter(str -> str.contains(String.valueOf(toFind))).count();
+    	System.out.println("total count of "+toFind + " is "+ count);
         
     }
 }

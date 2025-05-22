9446e18fb5ee00131e3f11683768951a35254cae
Md-Azam <faisalazam1999@gmail.com>
2025-05-22 09:55:57 +0530

    added some converstions in collections

--- src/collection/Covertions.java
+++ src/collection/Covertions.java
@@ -1,7 +1,11 @@
 package collection;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -19,6 +23,23 @@ public class Covertions {
 		int min = listInt.stream().min(Integer:: compareTo).get();
 		int max = listInt.stream().max(Integer:: compareTo).get();
 		System.out.println("Minimun in list is: "+ min + " and max in list is: "+ max);
+		
+		//List to set:
+		//Using treeset for sorted set
+		List<String> listForSet = Arrays.asList("apple", "banana", "apple", "orange");
+		Set<String> set = new TreeSet<>(listForSet);
+		System.out.println(set); // Output: [apple, banana, orange]
+		
+		//Using collectors.
+		Set<String> set2 = listForSet.stream().collect(Collectors.toSet());
+		System.out.println(set2); // Output: [banana, orange, apple]
+		
+		//List to set for Integers
+		List<Integer> intListSet = Arrays.asList(10,2,3,1,2,4,3,5,6);
+		Set<Integer> intSet = new HashSet<>(intListSet);
+//		Set<Integer> intSet = intListSet.stream().collect(Collectors.toSet());
+		System.out.println(intSet);
+
 		
 	}	
 

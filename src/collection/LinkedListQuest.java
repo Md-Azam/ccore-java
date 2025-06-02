faac8e9226ad5d3a22711cd78b59e389fbf5307d
Md-Azam <faisalazam1999@gmail.com>
2025-06-02 23:45:48 +0530

    linked list vs array list

--- src/collection/LinkedListQuest.java
+++ src/collection/LinkedListQuest.java
@@ -5,15 +5,15 @@ import java.util.Iterator;
 import java.util.LinkedList;
 
 public class LinkedListQuest {
-	
+
 	public static void main(String[] args) {
-		
+
 		ArrayList<Integer> list = new ArrayList<>();
 		list.add(1);
 		list.add(6);
 		list.add(56);
 		System.out.println(list);
-		
+
 		LinkedList<Integer> linkedList = new LinkedList<>();
 		linkedList.add(1);
 		linkedList.add(6);
@@ -23,6 +23,13 @@ public class LinkedListQuest {
 		System.out.println(linkedList);
 		Iterator it = linkedList.iterator();
 		System.out.println(it.toString());
+
+		/*
+		 * 1. Use This Frequent random access (index-based) ArrayList 
+		 * 2. Frequent insertions/deletions in middle/start LinkedList
+		 * 3. Memory efficiency is important ArrayList 
+		 * 4. You need queue/deque operations LinkedList
+		 */
 	}
 
 }

54eb42a7552ebbcd6ee9181aab05eb52cd6d45ea
Md-Azam <faisalazam1999@gmail.com>
2025-09-01 21:50:59 +0530

    word by word string reversal

---
+++ src/stringsOp/RevertseWorkByWord.java
@@ -0,0 +1,20 @@
+package stringsOp;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RevertseWorkByWord {
+
+	public static void main(String[] args) {
+		String s = " apple , banana , orange , grapes ";
+
+		List<String> list = Arrays.stream(s.split(",")).map(String::trim).collect(Collectors.toList());
+
+		System.out.println(list);
+		String splitedString  = Arrays.stream(s.split(" ")).map(word -> new StringBuffer(word).reverse()).collect(Collectors.joining());
+		System.out.println(splitedString);
+	}
+
+}

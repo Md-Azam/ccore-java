19efa507bf5a974df34f7dd57b816923d94bdb71
Md-Azam <faisalazam1999@gmail.com>
2025-12-28 23:46:30 +0530

    solved string problem

--- src/streams/AnagramUsingStream.java
+++ src/streams/AnagramUsingStream.java
@@ -5,15 +5,15 @@ import java.util.stream.Stream;
 
 public class AnagramUsingStream {
 	public static void main(String[] args) {
-		String s1 = "care";
-		String s2 = "race";
-
-		s1 = Stream.of(s1.split("")).map(String::toUpperCase).sorted().collect(Collectors.joining());
-		System.out.println("s1: "+ s1);
-		s2 = Stream.of(s2.split("")).map(String::toUpperCase).sorted().collect(Collectors.joining());
-		System.out.println("s2: "+ s2);
-		if (s1.equals(s2) == true) {
-			System.out.println("String is An anagram" + "s1: " + s1 + " s2: " + s2);
+		String str1 = "care";
+		String str2 = "race";
+		
+		str1 = Stream.of(str1.split("")).map(String::toUpperCase).sorted().collect(Collectors.joining());
+		System.out.println("str1: "+ str1);
+		str2 = Stream.of(str2.split("")).map(String::toUpperCase).sorted().collect(Collectors.joining());
+		System.out.println("value of str2: "+ str2);
+		if (str1.equals(str2) == true) {
+			System.out.println("String is An anagram" + "s1: " + str1 + " s2: " + str2);
 		} else {
 			System.out.println("String is not Anagram");
 		}
